4023a15dda65bf2d66ac73dde249a38ba606bccc
cspeloso <chrispeloso2@gmail.com>
2025-01-27 19:03:04 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.port || 3000;
+
+// Middleware to parse json
+app.use(express.json());
+
+app.get('/api', (req,res) => {
+    res.json({ message: 'Welcome to my API!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on https://localhost:${PORT}`);
+});
