0a5fd65458e737a2e5be7eb98df6220f5900e522
cspeloso <chrispeloso2@gmail.com>
2025-01-29 20:33:11 -0500

    adding cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 3000;
+
+//  Enable CORS
+app.use(cors());
 
 // Middleware to parse json
 app.use(express.json());
